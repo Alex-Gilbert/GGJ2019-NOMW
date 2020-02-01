3b962cbf5a67c8591db746c149c405d6bcb3575d
Cagan <caganhawthorne@gmail.com>
2020-02-01 15:20:47 -0600

    move script

---
+++ Assets/Scripts/MoveScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(h, 0) * speed;
+    }
+}
