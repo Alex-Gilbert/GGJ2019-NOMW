b8c280c984db87a63364b61844ada0af07512b5c
Alex-Gilbert <alexander.paul.gilbert@gmail.com>
2020-02-01 19:10:24 -0600

    painting mini game almost there

---
+++ Assets/Scripts/ArtWorkShaderDispatcher.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+using UnityEngine.Serialization;
+
+public class ArtWorkShaderDispatcher : MonoBehaviour
+{
+    public ComputeShader Shader;
+    private int shaderKernel;
+    
+    public ComputeShader PaintShader;
+    private int paintShaderKernel;
+    
+    public CreateNoiseTexture noiseGenerator;
+    public Texture2D OriginalArtWork;
+    public float scale;
+
+    public Texture2D brushTexture;
+    public float brushScale;
+    
+    private RenderTexture _artWorkTexture;
+    private RenderTexture _paintLayer;
+    private RenderTexture _noiseLayer;
+
+    private Camera _camera;
+
+    private Color _paintColor;
+
+    private Vector2 _prevMousePosition;
+    private float _prevRotation;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _camera = Camera.main;
+
+        _noiseLayer = noiseGenerator.CreateTexture();
+        
+        _artWorkTexture = new RenderTexture(OriginalArtWork.width, OriginalArtWork.height, 0)
+        {
+            filterMode = FilterMode.Bilinear,
+            wrapMode = TextureWrapMode.Clamp,
+            enableRandomWrite = true,
+            
+        };
+        _artWorkTexture.Create();
+        
+        _paintLayer = new RenderTexture(OriginalArtWork.width, OriginalArtWork.height, 0)
+        {
+            filterMode = FilterMode.Bilinear,
+            wrapMode = TextureWrapMode.Clamp,
+            enableRandomWrite = true,
+            
+        };
+        _paintLayer.Create();
+        
+        shaderKernel = Shader.FindKernel("CSMain");
+        
+        Shader.SetTexture(shaderKernel, "Result", _artWorkTexture);
+        Shader.SetTexture(shaderKernel, "_ArtWork", OriginalArtWork);
+        Shader.SetTexture(shaderKernel, "_Paint", _paintLayer);
+        Shader.SetTexture(shaderKernel, "_Noise", _noiseLayer);
+        
+        transform.localScale = new Vector3(OriginalArtWork.width * scale,OriginalArtWork.height * scale, 1f);
+        GetComponent<MeshRenderer>().material.SetTexture("_MainTex", _artWorkTexture);
+
+        paintShaderKernel = PaintShader.FindKernel("CSMain");
+        PaintShader.SetTexture(paintShaderKernel, "Result", _paintLayer);
+        
+        
+        _paintColor = Color.white;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+            _paintColor = Color.red;
+        
+        if(Input.GetKeyDown(KeyCode.Alpha2))
+            _paintColor = Color.green;
+        
+        if(Input.GetKeyDown(KeyCode.Alpha3))
+            _paintColor = Color.blue;
+        
+        if(Input.GetKeyDown(KeyCode.Alpha4))
+            _paintColor = Color.white;
+        
+        if(Input.GetKeyDown(KeyCode.Alpha5))
+            _paintColor = Color.black;
+        
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit hit;
+            Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform == transform)
+                {
+                    var point = hit.point;
+                    point.z = 0;
+
+                    var center = transform.position;
+                    center.z = 0;
+
+                    var right = transform.right * 2.0f;
+                    var up = transform.up * 2.0f;
+
+                    var x = Vector3.Dot(right, point - center) / transform.localScale.x;
+                    var y = Vector3.Dot(up, point - center) / transform.localScale.x;
+
+                    
+                    float rot;
+                    
+                    Debug.Log(Vector2.Distance(_prevMousePosition, Input.mousePosition) > 1f);
+                    
+                    if (Vector2.Distance(_prevMousePosition, Input.mousePosition) > 1f)
+                    {
+                        rot = Mathf.Atan2(Input.mousePosition.y - _prevMousePosition.y,
+                            Input.mousePosition.x - _prevMousePosition.x);
+                    }
+                    else
+                    {
+                        rot = _prevRotation;
+                    }
+                    
+                    Debug.Log(rot);
+                    
+                    PaintShader.SetFloats("drawColor", _paintColor.r, _paintColor.g, _paintColor.b);
+                    PaintShader.SetFloats("drawPosition", x, y);
+                    PaintShader.SetFloat("brushScale", brushScale);
+                    PaintShader.SetFloat("brushRotation", rot);
+                    PaintShader.SetTexture(paintShaderKernel, "_Brush", brushTexture);
+                    PaintShader.Dispatch(paintShaderKernel, OriginalArtWork.width / 8, OriginalArtWork.height / 8, 1);
+
+                    _prevMousePosition = Input.mousePosition;
+                    _prevRotation = rot;
+                    
+                    
+                }
+            }
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            RaycastHit hit;
+            Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform == transform)
+                {
+                    var point = hit.point;
+                    point.z = 0;
+
+                    var center = transform.position;
+                    center.z = 0;
+
+                    var right = transform.right * 2.0f;
+                    var up = transform.up * 2.0f;
+
+                    var x = Vector3.Dot(right, point - center) / transform.localScale.x;
+                    var y = Vector3.Dot(up, point - center) / transform.localScale.x;
+
+                    y /= OriginalArtWork.height / OriginalArtWork.width;
+                    y /= 2.0f;
+                    x /= 2.0f;
+                    x += 0.5f;
+                    y += 0.5f;
+                    x *= OriginalArtWork.width;
+                    y *= OriginalArtWork.height;
+                    
+                    var texture = new Texture2D(OriginalArtWork.width, OriginalArtWork.height);
+                    
+                    RenderTexture.active = _artWorkTexture;
+                    texture.ReadPixels (new Rect (0, 0, OriginalArtWork.width, OriginalArtWork.height), 0, 0);
+                    texture.Apply ();
+                    RenderTexture.active = null;
+
+                    _paintColor = texture.GetPixel((int) x, (int) y);
+                    
+                    Destroy(texture);
+                }
+            }
+        }
+
+        Shader.Dispatch(shaderKernel, OriginalArtWork.width / 8, OriginalArtWork.height / 8, 1);
+    }
+}
---
+++ Assets/Scripts/CreateNoiseTexture.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateNoiseTexture : MonoBehaviour
+{
+    public ComputeShader GenerateNoiseShader;
+    public ComputeShader DirectionalBlurShader;
+    private int generateNoiseKernel;
+    private int directionalBlurKernel;
+
+    private RenderTexture output;
+    public Texture2D[] grungeMaps;
+    public Texture2D[] dirtMaps;
+    public Texture2D[] cellsMaps;
+    public Texture2D[] noiseMaps;
+
+    public float NoisePosition;
+    public float NoiseContrast;
+    
+    public float GrungePosition;
+    public float GrungeContrast;
+    
+    public RenderTexture CreateTexture()
+    {
+        generateNoiseKernel = GenerateNoiseShader.FindKernel("CSMain");
+        directionalBlurKernel = DirectionalBlurShader.FindKernel("CSMain");
+
+        output = new RenderTexture(1024, 1024, 0)
+        {
+            filterMode = FilterMode.Bilinear,
+            wrapMode = TextureWrapMode.Clamp,
+            enableRandomWrite = true,
+            
+        };
+        output.Create();
+        
+        GenerateNoiseShader.SetTexture(generateNoiseKernel, "Result", output);
+
+        var g = grungeMaps[Random.Range(0, grungeMaps.Length)];
+        GenerateNoiseShader.SetTexture(generateNoiseKernel, "GrungeMap", grungeMaps[Random.Range(0, grungeMaps.Length)]);
+        GenerateNoiseShader.SetTexture(generateNoiseKernel, "DirtMap", dirtMaps[Random.Range(0, dirtMaps.Length)]);
+        GenerateNoiseShader.SetTexture(generateNoiseKernel, "CellsMap", cellsMaps[Random.Range(0, cellsMaps.Length)]);
+        GenerateNoiseShader.SetTexture(generateNoiseKernel, "PerlinNoise", noiseMaps[Random.Range(0, noiseMaps.Length)]);
+        
+        GenerateNoiseShader.SetFloat("grungeContrast", GrungeContrast);
+        GenerateNoiseShader.SetFloat("grungeContrastPosition", GrungePosition);
+        
+        GenerateNoiseShader.SetFloat("noiseContrast", NoiseContrast);
+        GenerateNoiseShader.SetFloat("noiseContrastPosition", NoisePosition);
+        
+        GenerateNoiseShader.Dispatch(generateNoiseKernel, output.width/8, output.height/8, 1);
+
+        return output;
+    }
+
+    void Dispose()
+    {
+        
+    }
+}
