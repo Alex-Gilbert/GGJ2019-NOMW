207f22fe3030293fa7f790e05aeeeafec6c45eb9
Alex-Gilbert <alexander.paul.gilbert@gmail.com>
2020-02-02 09:19:18 -0600

    fixed it

--- Assets/Scripts/GetTextureFromTag.cs
+++ Assets/Scripts/GetTextureFromTag.cs
@@ -18,28 +18,5 @@ public class GetTextureFromTag : MonoBehaviour
         }
     }
 
-    public void SaveImage()
-    {
-        var t = GameObject.FindWithTag(tag);
-        if (t != null)
-        {
-            var a = t.GetComponent<ArtPiece>();
-
-            var aw = a.GetPainting() as RenderTexture;
-            
-            var tex = new Texture2D(aw.width, aw.height);
-            
-            RenderTexture.active = aw;
-            tex.ReadPixels(new Rect(0, 0, aw.width, aw.height), 0, 0);
-            tex.Apply();
-            RenderTexture.active = null;
-            
-            // Encode texture into PNG
-            byte[] bytes = tex.EncodeToPNG();
-            Object.Destroy(tex);
-
-            // For testing purposes, also write to a file in the project folder
-            File.WriteAllBytes(Application.dataPath + $"/../{tag}.png", bytes);
-        }
-    }
+   
 }
